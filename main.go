4b9205c27574a2615ef96b3bc595febfda97c54f
Nathan Dao <nathan@guynathan.com>
2016-07-23 10:50:36 +0700

    test

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/google/cayley"
+	"github.com/google/cayley/graph/bolt"
 
 	"fmt"
 )
 
 func main() {
+	bolt.init()
 	store, err := cayley.NewGraph("bolt", "/Users/nathan/vantaa.db", nil)
 	if err != nil {
 		fmt.Println(err)
